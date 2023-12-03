454a5c23ce9b61beb10084935674f278003e89fa
tabaochoke <108040902+tabaochoke@users.noreply.github.com>
2023-12-03 11:42:02 +0700

    add khach hang and nhan vien

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify,session, make_response
 import pypyodbc as obdc
+##NGHIA NGU
+# DRIVER_NAME = 'SQL SERVER'
+# SERVER_NAME = 'TRONG-NGHIA\MSSQLSERVER01'
+# DATABASE_NAME ='HQT_CSDL'
+# connection_string = f'DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME}'
+# conn = obdc.connect(connection_string)
 
+#BAO2MAI
 DRIVER_NAME = 'SQL SERVER'
-SERVER_NAME = 'TRONG-NGHIA\MSSQLSERVER01'
-DATABASE_NAME ='HQT_CSDL'
+SERVER_NAME = 'DESKTOP-S3ESUI2\BAOSERVER'
+DATABASE_NAME = 'HQT_CSDL'
 
-connection_string = f'DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME}'
-conn = obdc.connect(connection_string)
+connection_string = (
+    r'DRIVER={SQL Server};'
+    r'SERVER=DESKTOP-S3ESUI2\BAOSERVER;'
+    r'DATABASE=HQT_CSDL;'
+    r'Trusted_Connection=yes;'
+)
+conn = obdc.connect (connection_string)
+
+
 app = Flask(__name__)
-app.secret_key = 'nghia1804'
+# app.secret_key = 'nghia1804'
 
 @app.route('/')
 def base():
@@ -27,18 +41,25 @@ def login():
     #get fetch
     sdt = request.form['username']
     password = request.form['password']
-
+    print (sdt,password)
     #run query
     cursor.execute("{CALL CheckAccountExists(?, ?)}", (sdt, password))
     rows = cursor.fetchall()
-    cursor.close()
     print(rows[0][0])
+    cursor.close()
+   
     if rows[0][0]=='Admin':
         # print("after:" ,session)
         return jsonify({'redirect': '/admin'})
     elif rows[0][0] == 'BacSi':
         # print("after:" ,session)
-        return jsonify({'redirect': '/dentist'})        
+        return jsonify({'redirect': '/dentist'})       
+    elif rows[0][0] == 'NhanVien':
+        # print("after:" ,session)
+        return jsonify({'redirect': '/staff'})
+    elif rows[0][0] == 'Khach':
+        print("after:" ,session)
+        return jsonify({'redirect': '/user'})
     return jsonify({'message': 'Tên người dùng hoặc mật khẩu không đúng'})
 
 @app.route('/alert')
@@ -95,6 +116,12 @@ def submit_form():
         cursor.close()
         return jsonify({'status': 'success', 'message': 'Đăng ký thành công'})
     
+    
+    
+    
+    
+    
+    
 # @app.route('/check_meeting', methods=['POST'])
 # def check_meeting():
 #     if request.method == 'POST':
@@ -115,5 +142,55 @@ def submit_form():
         
 #         cursor.close()
 #         return jsonify(meetings_on_day)
+
+
+@app.route('/staff')
+def dat_lich_hen():
+  return render_template('./nhanvien/dat_lich_hen.html')
+@app.route('/xuat_hoa_don')
+def xuat_hoa_don():
+  return render_template('./nhanvien/xuat_hoa_don.html')
+
+@app.route('/tao_tai_khoan')
+def tao_tai_khoan():
+  return render_template('./nhanvien/tao_tai_khoan.html')
+
+@app.route('/xem_ttthuoc')
+def xem_ttthuoc():
+  return render_template('./nhanvien/xem_ttthuoc.html')
+
+@app.route('/lichkham')
+def lichkham():
+  return render_template('./nhanvien/lichkham.html')
+
+@app.route('/hosokhambenh')
+def hosokhambenh():
+  return render_template('./nhanvien/hosokhambenh.html')
+
+
+#Khach hang
+@app.route('/aa')
+def dangkitaikhoan():
+    return render_template('./khachhang/dangkitaikhoan.html')
+
+@app.route('/user')
+def xemttcanhan():
+    return render_template('./khachhang/thongtincanhan.html')
+
+
+@app.route('/datlichhen')
+def datlichhen():
+    return render_template('./khachhang/datlichhen.html')
+
+
+@app.route('/xemhsbenhan')
+def xemhsbenhan():
+    return render_template('./khachhang/xemhsbenhan.html')
+
+@app.route('/xemlichkham')
+def xemlichkham():
+    return render_template('./khachhang/xemlichkham.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
