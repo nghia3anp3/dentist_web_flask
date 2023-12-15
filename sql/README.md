c92493079e1bdbbe832887254d3c1b3c91bdb1fb
vmthu-xii <vmthu.xii@gmail.com>
2023-12-15 14:38:01 +0700

    add some proc and readme sql

--- sql/README.md
+++ sql/README.md
@@ -7,26 +7,25 @@ Bao gồm 2 file:
 #### Các procedure có trong procedure.sql:
 
 - **sp_KiemTraSdtTonTai**: 
-    - Tham số truyền vào: @SDT VARCHAR(20)
+    - Tham số truyền vào: *@SDT VARCHAR(20)*
     - Kết quả trả về: giá trị 1 hoặc 0 (kiểu dữ liệu bit)
     - Chức năng: kiểm tra một số điện thoại đã tồn tại trong bảng NGUOIDUNG hay chưa.
 
 - **sp_KiemTraTKTonTai**:
-    - Tham số truyền vào *@SDT VARCHAR(20), @LoaiND varchar(10)*.
+    - Tham số truyền vào *@SDT VARCHAR(20), @LoaiND VARCHAR(10)*.
     - Kết quả trả về: giá trị 1 hoặc 0 (kiểu dữ liệu bit).
     - Chức năng: kiểm tra một số điện thoại đã được đăng ký với loại người dùng được truyền vào chưa.
     - Thường dùng trong việc đăng ký tài khoản.
 
 - **sp_LayThongTinTuSDT**:
-    - Tham số truyền vào: @SDT VARCHAR(20)
+    - Tham số truyền vào: *@SDT VARCHAR(20)*
     - Kết quả trả về: 
         - 1 bảng thông tin của người dùng sử dụng @SDT (thứ tự: HoTen, NgaySinh, DiaChi).
         - Hoặc trả về bảng có 1 cột 1 dòng duy nhất (cột result, value = 0).
     - Chức năng: Xem thông tin người dùng, kiểm tra xem có người dùng nào mang @SDT chưa, nếu có thì autofill thông tin.
 
-
 - **sp_LayHoaDonTuSDT**:
-    - Tham số truyền vào: @SDT VARCHAR(20)
+    - Tham số truyền vào: *@SDT VARCHAR(20)*
     - Kết quả trả về: 
         - 1 bảng chứa tất cả các hoá đơn của @SDT (thứ tự: *MaHoSo, MaHoaDon, NgayKham, MaNhaSi, TenNhaSi, MaThuoc, TenThuoc, SoLuongThuoc, DonGiaThuoc, MaDV, TenDV, SoLuongDV, DonGia, TongTien*). 
         - Hoặc trả về bảng có 1 cột 1 dòng duy nhất (cột result, value = 0). 
@@ -34,13 +33,12 @@ Bao gồm 2 file:
     - Chức năng: Lấy hoá đơn từ số điện thoại.
 
 - **sp_TimNhaSiRanh**:
-    - Tham số truyền vào: @Time varchar(15), @Date varchar(15).
+    - Tham số truyền vào: *@Time VARCHAR(15), @Date VARCHAR(15)*.
     - Kết quả trả về:
         - 1 bảng gồm MaNhaSi và TenNhaSi
         - Nếu không có thì trả về 1 bảng rỗng.
     - Chức năng: Tìm nha sĩ rảnh trong thời gian đầu vào.
     - Sử dụng: Khi đăng ký khám.
-
 
 - **sp_LayThongTinKhoThuoc**:
     - Tham số truyền vào: không có
@@ -50,7 +48,7 @@ Bao gồm 2 file:
     - Sử dụng: Chức năng xem danh sách thuốc của admin và QTV.
 
 - **sp_TimThuocBangTen**:
-    - Tham số truyền vào: @TenThuoc VARCHAR(20). (Có thể là 1 phần trong tên thuốc).
+    - Tham số truyền vào: *@TenThuoc VARCHAR(20)*. (Có thể là 1 phần trong tên thuốc).
     - Kết quả trả về:
         - 1 bảng chứa thông tin thuốc (Thứ tự: *MaThuoc,TenThuoc, DonGia, ChiDinh, SoLuongTon, NgayHetHan.*)
         - 1 bảng rỗng nếu không có tên thuốc tương ứng.
@@ -58,69 +56,145 @@ Bao gồm 2 file:
 
 - **sp_DatLichKham**:
     - Tham số truyền vào: 
-    *@SDT VARCHAR(20),	
-				            @HoTen nvarchar(30),
-				@NgaySinh varchar(15),
-				@DiaChi nvarchar(50),
-				@NgayHen varchar(15),
-				@GioHen varchar(15),
+                *@SDT VARCHAR(20),	
+				@HoTen NVARCHAR(30),
+				@NgaySinh VARCHAR(15),
+				@DiaChi NVARCHAR(50),
+				@NgayHen VARCHAR(15),
+				@GioHen VARCHAR(15),
 				@MaBacSi VARCHAR(20)*
-        **Lưu ý**: Với những trường không cần truyền (trong trường hợp có người dùng dùng SDT kia rồi) thì có thể truyền vào rỗng.
+    
+    **Lưu ý**: Với những trường không cần truyền (trong trường hợp có người dùng dùng SDT kia rồi) thì có thể truyền vào rỗng.
     - Kết quả trả về: Không có, procedure sẽ insert vào database (bảng LICHEN và NGUOIDDUNG (nếu chưa tồn tại SDT)).
     - Chức năng:
     - Sử dụng
 
 - **sp_ThemNguoiDung**:
     - Tham số truyền vào: 
-       *@SDT VARCHAR(20),	
-				@HoTen nvarchar(30),
-				@NgaySinh varchar(15),
-				@DiaChi nvarchar(50)*
+                *@SDT VARCHAR(20),	
+				@HoTen NVARCHAR(30),
+				@NgaySinh VARCHAR(15),
+				@DiaChi NVARCHAR(50)*
     - Kết quả trả về: 
         - 1 nếu đăng ký thành công.
         - 0 nếu đăng ký không thành công (trong trường hợp SDT đã tồn tại rồi)
     - Chức năng: Thêm người dùng vào database.
     - Sử dụng: đăng ký tài khoản, đăng ký lịch hẹn (chưa từng khám ở đây).
-    
-
 
 - **sp_DangKiTaiKhoan**:
     - Tham số truyền vào: 
-       @SDT varchar(10),
-				@HoTen nvarchar(30),
+                *@SDT VARCHAR(10),
+				@HoTen NVARCHAR(30),
 				@NgaySinh DATE,
-				@DiaChi nvarchar (50),
-				@MatKhau varchar(20),
-				@LoaiND varchar(10)*
-        **Lưu ý**: Với những trường không cần truyền (trong trường hợp có người dùng dùng SDT kia rồi) thì có thể truyền vào rỗng.
+				@DiaChi NVARCHAR (50),
+				@MatKhau VARCHAR(20),
+				@LoaiND VARCHAR(10)*
+    **Lưu ý**: Với những trường không cần truyền (trong trường hợp có người dùng dùng SDT kia rồi) thì có thể truyền vào rỗng.
     - Kết quả trả về: 
-        - 1 nếu đăng ký thành công.
-        - 0 nếu đăng ký không thành công (trong trường hợp SDT đã tồn tại rồi)
+        - 1: đăng ký thành công.
+        - 0: đăng ký không thành công (trong trường hợp SDT đã tồn tại rồi)
     - Chức năng: Thêm người dùng vào database.
     - Sử dụng: đăng ký tài khoản, đăng ký lịch hẹn (chưa từng khám ở đây).
-
-
-- **sp_DangKiTaiKhoan**:
-    - Tham số truyền vào: 
-       @SDT varchar(10),
-				@HoTen nvarchar(30),
-				@NgaySinh DATE,
-				@DiaChi nvarchar (50),
-				@MatKhau varchar(20),
-				@LoaiND varchar(10)*
-        **Lưu ý**: Với những trường không cần truyền (trong trường hợp có người dùng dùng SDT kia rồi) thì có thể truyền vào rỗng.
-    - Kết quả trả về: 
-        - 1 nếu đăng ký thành công.
-        - 0 nếu đăng ký không thành công (trong trường hợp SDT đã tồn tại rồi)
-    - Chức năng: Thêm người dùng vào database.
-    - Sử dụng: đăng ký tài khoản, đăng ký lịch hẹn (chưa từng khám ở đây).
-
-
 
 - **sp_XemLichKham**:
-    - Tham số truyền vào: 
-       @SDT varchar(10)*
+    - Tham số truyền vào: *@SDT VARCHAR(10)*
     - Kết quả trả về: 
         - Bảng thông tin các lịch khám (thứ tự: *NgayKham, GioKham, MaNhaSi, TenNhaSi, HoTen, SDT*) 
         - Bảng 1 dòng 1 cột (result: 0)
     - Chức năng: Xem lịch hẹn khám của @SDT.
+
+- **sp_XemHoSoBenhAn**:
+    - Tham số truyền vào: *@SDT VARCHAR(20)*.
+    - Kết quả trả về: Tất cả các *nhóm 3 bảng* thông tin theo thứ sau:
+        - Thông tin Hồ sơ bệnh án (*MaHoSo, NgayKham, MaNhaSi, TongTien*);
+        - Thông tin Đơn thuốc (*MaThuoc, SoLuong, DonGia*);
+        - Thông tin Đơn dịch vụ (*MaDV, SoLuong, DonGia*).
+    - Chức năng: Xem chi tiết hồ sơ bệnh án của @SDT.
+
+- **sp_CapNhatThongTin**
+    - Tham số truyền vào: *@SDT VARCHAR(20), @HoTen NVARCHAR(30), @NgaySinh DATE, @DiaChi NVARCHAR(50), @MatKhau VARCHAR(20)*.
+
+    **Lưu ý**: Không cho cập nhật *@SDT*, *@SDT, @HoTen* phải khác rỗng.
+
+    - Kết quả trả về: Tất cả các *nhóm 3 bảng* theo thứ tự:
+        - 1: cập nhật thông tin thành công;
+        - 0: thất bại (*@MatKhau* không đúng định dạng)
+    - Chức năng: Xem chi tiết hồ sơ bệnh án của @SDT.
+
+- **f_KTLichTrungNhau**
+    - Tham số truyền vào: *@NgayGioBD1 DATETIME, @NgayGioKT1 DATETIME, @NgayGioBD2 DATETIME, @NgayGioKT2 DATETIME*
+    - Kết quả trả về:
+        - 1: trùng
+        - 0: không trùng
+    - Chức năng: Kiểm tra xem 2 lịch có trùng (giao) nhau không
+
+- **f_KTLichHopLe**
+    - Tham số truyền vào: *@MaNhaSi VARCHAR(20), @NgayGioBD DATETIME, @NgayGioKT DATETIME*
+    - Kết quả trả về:
+        - 1: hợp lệ
+        - 0: không hợp lệ (có lịch trùng nhau)
+    - Chức năng: Kiểm tra lịch bận thêm vào có hợp lệ không
+
+- **sp_CapNhatLichBan**:
+    - Tham số truyền vào: *@MaNhaSi VARCHAR(20), @MALB INT, @NgayBDMoi VARCHAR(20), @GioBDMoi VARCHAR(20), @NgayKTMoi VARCHAR(20), @GioKTMoi VARCHAR(20)*
+    - Kết quả trả về: 
+        - 1: cập nhật thành công
+        - 0: cập nhật không thành công (f_KTLichHopLe = FALSE)
+    - Chức năng: Cập nhật (Sửa) lịch bận của @MaNhaSi
+
+- **sp_ThemLichBan**:
+    - Tham số truyền vào: *@MaNhaSi VARCHAR(20), @NgayBD VARCHAR(20), @GioBD VARCHAR(20), @NgayKT VARCHAR(20), @GioKT VARCHAR(20)*
+    - Kết quả trả về:
+        - 1: thêm thành công
+        - 0: thêm không thành công (f_KTLichHopLe = FALSE)
+    - Chức năng: Thêm lịch bận mới vào lịch của @MaNhaSi
+
+- **sp_ThanhToan**:
+    - Tham số truyền vào: *@SDT VARCHAR(20)*
+    - Kết quả trả về:
+        - 1: thanh toán thành công (không còn hồ sơ nào chưa thanh toán cũng trả về thành công)
+        - 0: thanh toán không thành công (@SDT không tồn tại)
+    - Chức năng: Thực hiện thanh toán hóa đơn chưa thanh toán của @SDT
+
+- **sp_TaoHoSoBenhAn**:
+    - Tham số truyền vào: *@SDT VARCHAR(20), @MaNhaSi VARCHAR(20)*
+    - Kết quả trả về: 
+        - 1: tạo thành công
+        - 0: không thành công (@SDT không tồn tại)
+    - Chức năng: Tạo hồ sơ bệnh án mới cho @SDT
+
+- **sp_TaoDonThuoc**:
+    - Tham số truyền vào: *@MaHoSo INT, @TenThuoc CHAR(30), @SoLuong INT*
+    - Kết quả trả về:
+        - 1: tạo thành công
+        - 0: không thành công (@SoLuong vượt quá số lượng tồn)
+    - Chức năng: Thêm thuốc vào @MaHoSo
+
+- **sp_TaoDonDV**:
+    - Tham số truyền vào: *@MaHoSo INT, @TenDV NVARCHAR(50)*
+    - Kết quả trả về: 
+    - Kết quả trả về:
+        - 1: tạo thành công
+        - 0: không thành công
+    - Chức năng: Thêm dịch vụ vào @MaHoSo
+
+- **sp_KhoaTaiKhoan**:
+    - Tham số truyền vào: *@SDT VARCHAR(20), @LoaiND VARCHAR(10)*
+    - Kết quả trả về:
+        - 1: khóa thành công
+        - 0: không thành công (khi @SDT không có tài khoản)
+    - Chức năng: Khóa tài khoản @SDT gặp vấn đề 
+
+- **sp_ThemThuoc**:
+    - Tham số truyền vào: *@MaThuoc CHAR(10), @TenThuoc CHAR(30),	@DonGia INT, @ChiDinh NVARCHAR(100), @SoLuongTon INT, @NgayHetHan DATE*
+    - Kết quả trả về: 
+        - 1: thêm thành công
+        - 0: không thành công (@MaThuoc đã tồn tại, @NgayHetHan trước ngày thêm)
+    - Chức năng: Thêm thuốc mới.
+
+- **sp_SuaThongTinThuoc**:
+    - Tham số truyền vào: *@MaThuoc CHAR(10), @TenThuoc CHAR(30),	@DonGia INT, @ChiDinh NVARCHAR(100), @SoLuongTon INT, @NgayHetHan DATE*
+    - Kết quả trả về:
+        - 1: sửa thành công
+        - 0: không thành công (@MaThuoc chưa tồn tại, @NgayHetHan trước ngày sửa)
+    - Chức năng: Sửa thông tin thuốc.
--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,8 +1,6 @@
 ﻿USE HQT_CSDL
 GO
 
-
-
 --KIEM TRA MOT SO DIEN THOAI CO TON TAI TRONG NGUOI DUNG HAY CHUA
 CREATE PROCEDURE sp_KiemTraSdtTonTai
 				@SDT VARCHAR(20)
@@ -24,7 +22,7 @@ PRINT @TEST
 GO
 
 
---KIEM TRA TAI KHOAN DA TON TAI HAY CHUA
+-- KIEM TRA TAI KHOAN DA TON TAI HAY CHUA
 CREATE PROCEDURE sp_KiemTraTKTonTai
 				@SDT varchar(20),
 				@LoaiND varchar(10)
@@ -101,7 +99,7 @@ BEGIN
 END;
 Go
 
-EXEC sp_LayHoaDonTuSDT '397'
+EXEC sp_LayHoaDonTuSDT '123'
 GO
 
 
@@ -200,10 +198,6 @@ GO
 EXEC sp_DatLichKham '000', N'Nguyễn Văn M','2003-11-11', N'Gia Lâm, Hà Nội', '2020-01-10', '15:00', '397'
 GO
 
-
-
-use HQT_CSDL
-GO
 -- THEM NGUOI DUNG (CO KIEM TRA NGUOI DUNG CO TON TAI HAY CHUA)
 CREATE PROCEDURE sp_ThemNguoiDung
 				@SDT VARCHAR(20),	
@@ -307,4 +301,383 @@ END;
 GO
 
 EXEC sp_XemLichKham '123'
+GO
+
+-- =========== Trang =========== --
+
+-- (KhachHang) XEM HO SO BENH AN --
+CREATE OR ALTER PROCEDURE sp_XemHoSoBenhAn 
+	@SDT VARCHAR(20)
+AS
+    DECLARE @DS_BENHAN TABLE(MaHoSo int, NgayKham datetime, MaNhaSi VARCHAR(20), TongTien bigint)
+	INSERT INTO @DS_BENHAN
+		SELECT HS.MaHoSo, HS.NgayKham, HS.MaNhaSi, HS.TongTien
+		FROM HOSOBENHAN HS 
+		WHERE HS.SDT = @SDT
+    
+    DECLARE @SL_BENHAN int
+	SELECT @SL_BENHAN = COUNT(*) FROM @DS_BENHAN
+
+    IF (@SL_BENHAN = 0)
+    BEGIN
+        SELECT NULL AS MaHoSo
+        RETURN 
+    END 
+
+    DECLARE @i int, @MaHoSo int, @NgayKham datetime, @MaNhaSi VARCHAR(20), @TongTien bigint
+    SET @i = 0
+
+    WHILE (@i < @SL_BENHAN)
+	BEGIN
+        SELECT '==================== Ho So ' + CAST(@i+1 as char(3)) + '====================' AS STT
+        SELECT TOP 1 @MaHoSo = MaHoSo FROM @DS_BENHAN
+        -- In Ho So --
+        SELECT TOP 1 * FROM @DS_BENHAN
+        -- In Don Thuoc --
+        SELECT '--- Don Thuoc ---'
+        SELECT MaThuoc, SoLuong, DonGia
+        FROM DONTHUOC 
+        WHERE MaDonThuoc = @MaHoSo
+        
+        -- In Don Dich Vu --
+        SELECT '--- Don Dich Vu ---'
+        SELECT MaDV, DonGia
+        FROM DONDICHVU 
+        WHERE MaDonDV = @MaHoSo
+
+        SET @i = @i + 1	
+		DELETE @DS_BENHAN WHERE MaHoSo = @MaHoSo
+	END
+GO
+
+-- EXEC sp_XemHoSoBenhAn '001'
+-- GO
+-- EXEC sp_XemHoSoBenhAn '000'
+-- GO
+EXEC sp_XemHoSoBenhAn '123'
+GO
+
+-- (KhachHang) CAP NHAT THONG TIN --
+CREATE OR ALTER PROCEDURE sp_CapNhatThongTin 
+    @SDT varchar(20), @HoTen nvarchar(30), @NgaySinh date, @DiaChi nvarchar(50), @MatKhau varchar(20)
+AS
+    IF (@HoTen <> '' AND @MatKhau <> '')
+    BEGIN
+        UPDATE NGUOIDUNG SET HoTen = @HoTen, NgaySinh = @NgaySinh, DiaChi = @DiaChi WHERE SDT = @SDT
+        UPDATE TAIKHOAN SET MatKhau = @MatKhau WHERE SDT = @SDT
+        RETURN 1
+    END ELSE
+    BEGIN
+        RAISERROR('Ho ten va Mat khau phai khac rong', 16, 1)
+		RETURN 0
+    END
+GO
+
+-- EXEC sp_CapNhatThongTin '630', '', '2003-06-20', N'Bảo Lộc, Lâm Đồng', 'trang'
+-- GO
+EXEC sp_CapNhatThongTin '630', 'Võ Thu Trang', '2003-06-20', N'Bảo Lộc, Lâm Đồng', 'trang'
+GO
+
+-- function: KIEM TRA HAI LICH CO TRUNG NHAU KHONG (0: khong trung, 1: trung) --
+CREATE FUNCTION f_KTLichTrungNhau 
+    (@NgayGioBD1 datetime, @NgayGioKT1 datetime, @NgayGioBD2 datetime, @NgayGioKT2 datetime)
+RETURNS bit 
+AS
+BEGIN
+    IF (@NgayGioKT1 < @NgayGioBD2)
+        RETURN 0
+    IF (@NgayGioKT2 < @NgayGioBD1)
+        RETURN 0
+    RETURN 1
+END
+GO
+
+print dbo.f_KTLichTrungNhau('2023-12-02 12:30:00', '2023-12-02 14:30:00', 
+                            '2023-12-01 15:30:00', '2023-12-02 13:30:00')
+GO
+
+-- function: KIEM TRA LICH BAN CO HOP LE KHONG --
+CREATE FUNCTION f_KTLichHopLe 
+	(@MaNhaSi varchar(20), @NgayGioBD datetime, @NgayGioKT datetime)
+RETURNS bit 
+AS
+BEGIN
+    IF (@NgayGioBD >= @NgayGioKT)
+    BEGIN
+		RETURN 0
+	END
+
+    -- Kiem tra trong LICHHEN cua @MaNhaSi co bi trung khong --
+    DECLARE @DS_LICHHEN TABLE(MaLH int, ThoiGianHen datetime)
+	INSERT INTO @DS_LICHHEN
+		SELECT MaLH, ThoiGianHen
+		FROM LICHHEN 
+		WHERE MaNhaSi = @MaNhaSi
+    
+    DECLARE @SL_LICHHEN int
+	SELECT @SL_LICHHEN = COUNT(*) FROM @DS_LICHHEN
+
+    IF (@SL_LICHHEN = 0)
+        RETURN 1
+
+    DECLARE @i int, @MaLH int, @NgayKhamBD datetime, @NgayKhamKT datetime
+    SET @i = 0
+
+    WHILE (@i < @SL_LICHHEN)
+	BEGIN
+        SELECT TOP 1 @MaLH = MaLH, @NgayKhamBD = ThoiGianHen FROM @DS_LICHHEN
+        SET @NgayKhamKT = DATEADD(hour, 1, @NgayKhamBD)
+        
+        IF (dbo.f_KTLichTrungNhau(@NgayKhamBD, @NgayKhamKT, @NgayGioBD, @NgayGioKT) = 1)
+            RETURN 0
+
+        SET @i = @i + 1
+		DELETE @DS_LICHHEN WHERE MaLH = @MaLH
+	END
+
+    RETURN 1
+END
+GO
+
+print dbo.f_KTLichHopLe('237', '2023-12-02 10:30', '2023-12-02 13:30')
+GO
+
+-- (NhaSi) CAP NHAT LICH BAN --
+CREATE OR ALTER PROCEDURE sp_CapNhatLichBan
+    @MaNhaSi varchar(20), @MALB int, @NgayBDMoi varchar(20), @GioBDMoi varchar(20), @NgayKTMoi varchar(20), @GioKTMoi varchar(20)
+AS
+    DECLARE @NgayGioBD_Moi DATETIME = CONVERT(DATETIME, @NgayBDMoi + ' ' + LEFT(@GioBDMoi,2) + ':00')
+    DECLARE @NgayGioKT_Moi DATETIME = CONVERT(DATETIME, @NgayKTMoi + ' ' + LEFT(@GioKTMoi,2) + ':00')
+
+    IF (dbo.f_KTLichHopLe(@MaNhaSi, @NgayGioBD_Moi, @NgayGioKT_Moi) = 0)
+    BEGIN
+		RAISERROR('Cap nhat khong thanh cong', 16, 1)
+		RETURN 0
+	END
+
+    UPDATE LICHBAN SET NgayGioBatDau = @NgayGioBD_Moi, NgayGioKetThuc = @NgayGioKT_Moi WHERE MALB = @MALB
+    RETURN 1
+GO
+
+EXEC sp_CapNhatLichBan '237', 5, '2023-12-02', '09:00', '2023-12-02', '10:00'
+GO
+
+-- (NhaSi) THEM LICH BAN --
+CREATE OR ALTER PROCEDURE sp_ThemLichBan
+    @MaNhaSi varchar(20), @NgayBD varchar(20), @GioBD varchar(20), @NgayKT varchar(20), @GioKT varchar(20)
+AS
+    DECLARE @NgayGioBD DATETIME = CONVERT(DATETIME, @NgayBD + ' ' + LEFT(@GioBD,2) + ':00')
+    DECLARE @NgayGioKT DATETIME = CONVERT(DATETIME, @NgayKT + ' ' + LEFT(@GioKT,2) + ':00')
+
+    IF (dbo.f_KTLichHopLe(@MaNhaSi, @NgayGioBD, @NgayGioKT) = 0)
+    BEGIN
+		RAISERROR('Them khong thanh cong', 16, 1)
+		RETURN 0
+	END
+
+	DECLARE @MALB int = (SELECT ISNULL(MAX(MALB),0) FROM LICHBAN) + 1
+	
+	INSERT INTO LICHBAN VALUES (@MALB, @MaNhaSi, @NgayGioBD, @NgayGioKT)
+	RETURN 1
+GO
+
+EXEC sp_ThemLichBan '237', '2023-12-03', '06:00', '2023-12-03', '08:00'
+GO
+EXEC sp_ThemLichBan '237', '2023-12-03', '09:00', '2023-12-03', '10:00'
+GO
+
+-- (NhanVien) THANH TOAN --
+CREATE OR ALTER PROCEDURE sp_ThanhToan 
+	@SDT varchar(20)
+AS
+	DECLARE @isSDTExist BIT
+	EXEC @isSDTExist = sp_KiemTraSdtTonTai @SDT
+
+	IF (@isSDTExist = 0)
+	BEGIN 
+		RAISERROR('SDT khong ton tai', 16, 1)
+		RETURN 0
+	END
+
+	DECLARE @DS_CANTHANHTOAN TABLE(MaHoSo int)
+	INSERT INTO @DS_CANTHANHTOAN
+		SELECT MaHoSo
+		FROM HOSOBENHAN 
+		WHERE SDT = @SDT AND TongTien = 0
+    
+    DECLARE @SL_TT int
+	SELECT @SL_TT = COUNT(*) FROM @DS_CANTHANHTOAN
+
+    IF (@SL_TT = 0)
+	BEGIN
+		print('Khong co ho so can thanh toan')
+        RETURN 1
+	END
+
+    DECLARE @i int, @MaHoSo int, @TongTienThuoc bigint, @TongTienDV bigint
+    SET @i = 0
+
+    WHILE (@i < @SL_TT)
+	BEGIN
+        SELECT TOP 1 @MaHoSo = MaHoSo FROM @DS_CANTHANHTOAN
+		SELECT @TongTienThuoc = SUM(DonGia * SoLuong) FROM DONTHUOC WHERE MaDonThuoc = @MaHoSo
+		SELECT @TongTienDV = SUM(DonGia) FROM DONDICHVU WHERE MaDonDV = @MaHoSo
+		-- Thanh toan --
+		UPDATE HOSOBENHAN SET TongTien = @TongTienThuoc + @TongTienDV WHERE MaHoSo = @MaHoSo
+
+        SET @i = @i + 1
+		DELETE @DS_CANTHANHTOAN WHERE MaHoSo = @MaHoSo
+	END
+	RETURN 1
+GO
+
+EXEC sp_ThanhToan '234'
+GO
+EXEC sp_ThanhToan '123'
+GO
+
+-- (NhaSi) TAO HO SO BENH AN --
+CREATE OR ALTER PROCEDURE sp_TaoHoSoBenhAn 
+	@SDT varchar(20), @MaNhaSi varchar(20)
+AS
+	DECLARE @isSDTExist BIT
+	EXEC @isSDTExist = sp_KiemTraSdtTonTai @SDT
+
+	IF (@isSDTExist = 0)
+	BEGIN 
+		RAISERROR('SDT khong ton tai', 16, 1)
+		RETURN 0
+	END
+
+	DECLARE @MaHoSo int = (SELECT ISNULL(MAX(MaHoSo),0) FROM HOSOBENHAN) + 1
+	INSERT INTO HOSOBENHAN(MaHoSo, SDT, MaNhaSi, NgayKham, TongTien) VALUES (@MaHoSo, @SDT, @MaNhaSi, GETDATE(), 0)
+	RETURN 1
+GO
+
+EXEC sp_TaoHoSoBenhAn '234', '397'
+GO
+
+-- (NhaSi) TAO DON THUOC --
+CREATE OR ALTER PROCEDURE sp_TaoDonThuoc 
+	@MaHoSo int, @TenThuoc char(30), @SoLuong int
+AS
+	DECLARE @SoLuongTon int 
+	SELECT @SoLuongTon = SoLuongTon FROM THUOC WHERE TenThuoc = @TenThuoc
+
+	IF (@SoLuong > @SoLuongTon)
+	BEGIN
+		RAISERROR('Vuot qua so luong ton', 16, 1)
+        RETURN 0
+	END
+
+	DECLARE @MaThuoc char(10), @DonGia int
+	SELECT @MaThuoc = MaThuoc, @DonGia = DonGia FROM THUOC WHERE TenThuoc = @TenThuoc
+
+	INSERT INTO DONTHUOC(MaDonThuoc, MaThuoc, SoLuong, DonGia) VALUES (@MaHoSo, @MaThuoc, @SoLuong, @DonGia)
+	UPDATE THUOC SET SoLuongTon = @SoLuongTon - @SoLuong WHERE MaThuoc = @MaThuoc
+	RETURN 1
+GO
+
+EXEC sp_TaoDonThuoc 7, 'Panadol', 6
+GO
+
+-- (NhaSi) TAO DON DICH VU --
+CREATE OR ALTER PROCEDURE sp_TaoDonDV 
+	@MaHoSo int, @TenDV nvarchar(50)
+AS
+	DECLARE @MaDV char(10), @DonGia int
+	SELECT @MaDV = MaDV, @DonGia = DonGia FROM DICHVU WHERE TenDV = @TenDV
+
+	INSERT INTO DONDICHVU(MaDonDV, MaDV, DonGia) VALUES (@MaHoSo, @MaDV, @DonGia)
+	RETURN 1
+GO
+
+EXEC sp_TaoDonDV 7, N'Tẩy trắng răng'
+GO
+
+-- (QTV) KHOA TAI KHOAN --
+CREATE OR ALTER PROCEDURE sp_KhoaTaiKhoan
+	@SDT varchar(20), @LoaiND varchar(10)
+AS
+	-- Kiem tra da co tai khoan chua
+	DECLARE @isTKExist BIT
+	EXEC @isTKExist = sp_KiemTraTKTonTai @SDT, @LoaiND
+
+	IF (@isTKExist = 0)
+	BEGIN
+		RAISERROR('Tai khoan khong ton tai', 16, 1)
+        RETURN 0
+	END
+
+	-- Khoa
+	UPDATE TAIKHOAN SET TrangThai = 0 WHERE SDT = @SDT AND LoaiND = @LoaiND
+	RETURN 1
+GO
+
+EXEC sp_KhoaTaiKhoan '020', 'NhanVien'
+GO
+
+-- (QTV) THEM THUOC --
+CREATE OR ALTER PROCEDURE sp_ThemThuoc
+	@MaThuoc char(10), @TenThuoc char(30),	@DonGia int, @ChiDinh nvarchar(100), @SoLuongTon int, @NgayHetHan date
+AS
+	-- Kiem tra thuoc co ton tai khong
+	IF (@MaThuoc IN (SELECT MaThuoc FROM THUOC))
+	BEGIN
+		RAISERROR('Ma thuoc da ton tai', 16, 1)
+        RETURN 0
+	END
+
+	-- Kiem tra ngay het han co sau ngay them khong
+	IF (@NgayHetHan <= CAST(GETDATE() AS date))
+	BEGIN
+		RAISERROR('Ngay het han phai sau ngay them', 16, 1)
+        RETURN 0
+	END
+
+	-- Them
+	INSERT INTO THUOC(MaThuoc, TenThuoc, SoLuongTon, DonGia, NgayHetHan, ChiDinh) 
+	VALUES (@MaThuoc, @TenThuoc, @SoLuongTon, @DonGia, @NgayHetHan, @ChiDinh)
+	RETURN 1
+GO
+
+EXEC sp_ThemThuoc 'T006', 'Alaxan', 7000, N'Thuốc giảm đau kháng viêm', 100, '2025-12-30'
+GO
+EXEC sp_ThemThuoc 'T001', 'Paracetamol', 10000, N'Thuốc giảm đau hạ sốt', 100, '2025-12-30'
+GO
+
+-- (QTV) SUA THONG TIN THUOC --
+CREATE OR ALTER PROCEDURE sp_SuaThongTinThuoc
+	@MaThuoc char(10), @TenThuoc char(30),	@DonGia int, @ChiDinh nvarchar(100), @SoLuongTon int, @NgayHetHan date
+AS
+	-- Kiem tra thuoc can sua co ton tai khong
+	IF (@MaThuoc NOT IN (SELECT MaThuoc FROM THUOC))
+	BEGIN
+		RAISERROR('Ma thuoc khong ton tai', 16, 1)
+        RETURN 0
+	END
+
+	-- Kiem tra ten thuoc da ton tai chua
+	IF (@TenThuoc IN (SELECT TenThuoc FROM THUOC))
+	BEGIN
+		RAISERROR('Ten thuoc da ton tai', 16, 1)
+        RETURN 0
+	END
+
+	-- Kiem tra ngay het han co sau ngay sua khong
+	IF (@NgayHetHan <= CAST(GETDATE() AS date))
+	BEGIN
+		RAISERROR('Ngay het han phai sau ngay sua', 16, 1)
+        RETURN 0
+	END
+
+	-- Them
+	UPDATE THUOC SET TenThuoc = @TenThuoc, SoLuongTon = @SoLuongTon, DonGia = @DonGia, NgayHetHan = @NgayHetHan, ChiDinh = @ChiDinh
+	WHERE MaThuoc = @MaThuoc
+	RETURN 1
+GO
+
+EXEC sp_SuaThongTinThuoc 'T001', 'Panadol', 6000, N'Thuốc giảm đau kháng viêm', 100, '2025-12-30'
+GO
+EXEC sp_SuaThongTinThuoc 'T006', 'Cilzec', 8000, N'Thuốc điều trị cao huyết áp', 100, '2025-12-30'
 GO
