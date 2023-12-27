d244a224f59da7d9d3e0478e6de2fb7cd02673e9
tabaochoke <108040902+tabaochoke@users.noreply.github.com>
2023-12-27 16:45:53 +0700

    ADD TRANSACTION LOST UPDATE DIRTY READ

---
+++ sql/Transaction/21120205/LOSTUPDATE/TRAN1.sql
@@ -0,0 +1,14 @@
+﻿USE HQT_CSDL
+GO
+--- B1 :
+SELECT * FROM tb_ThuocHienHanh() WHERE MaThuoc = 'T001'
+
+------------------------------------
+----- B2 :
+DECLARE @RT BIT
+exec @RT = USP_updatethuoc1 'T001' , 10
+IF @RT = 1
+	PRINT N'Cập nhật số lượng thuốc thất bại'
+ELSE
+	PRINT N'Cập nhật số lượng thuốc thành công'
+---
---
+++ sql/Transaction/21120205/LOSTUPDATE/TRAN2.sql
@@ -0,0 +1,17 @@
+﻿USE HQT_CSDL
+GO
+
+-------------B1 :
+--- SELECT * FROM THUOC with (Updlock) WHERE TenThuoc = 'Paracetamol'
+---SELECT * FROM THUOC  WHERE TenThuoc = 'Paracetamol'
+
+DECLARE @RT BIT
+exec @RT = sp_TaoDonThuoc1 '7' , 'Paracetamol' , 10
+IF @RT = 1
+	PRINT N'Tạo đơn thuốc thất bại'
+ELSE
+	PRINT N'Tạo đơn thuốc thành công'
+
+SELECT * FROM tb_ThuocHienHanh() WHERE TenThuoc = 'Paracetamol'
+
+
