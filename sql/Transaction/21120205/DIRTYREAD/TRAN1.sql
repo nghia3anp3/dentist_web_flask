3001acef9959bbda2a7e9ef512f1f05ca66c21d6
tabaochoke <108040902+tabaochoke@users.noreply.github.com>
2023-12-28 19:25:28 +0700

    bao_web_flask

--- sql/Transaction/21120205/DIRTYREAD/TRAN1.sql
+++
@@ -1,9 +0,0 @@
-﻿USE HQT_CSDL
-GO
----- T001 Paracetamol
-SELECT * FROM tb_ThuocHienHanh() WHERE MaThuoc = 'T001'
---GO
-
-DECLARE @RT INT
-EXEC @RT =	USP_qtvupdatethuoc 'T001' , 10
-IF @RT = 1	PRINT N'CẬP NHẬT THẤT BẠI'ELSE	PRINT N'CẬP NHẬT THÀNH CÔNG'SELECT * FROM tb_ThuocHienHanh() WHERE MaThuoc = 'T001'
--- sql/Transaction/21120205/DIRTYREAD/TRAN2.sql
+++
@@ -1,7 +0,0 @@
-﻿USE HQT_CSDL
-GO
-
-DECLARE @RT FLOAT
-EXEC usp_nhasitaodonthuoc 7 , 'Paracetamol' , 6 , @RT OUT
-
-PRINT N'SỐ Lượng thuốc còn lại ' + CAST(@RT AS VARCHAR(20))
