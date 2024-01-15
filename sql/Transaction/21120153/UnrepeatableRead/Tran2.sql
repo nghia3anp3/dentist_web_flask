212010f404a9996ab11eddf0f84ba47bf5f5190e
Võ Thu Trang <102519408+voxthutrang@users.noreply.github.com>
2024-01-15 17:03:45 +0700

    trans Trang

--- sql/Transaction/21120153/UnrepeatableRead/Tran2.sql
+++ sql/Transaction/21120153/UnrepeatableRead/Tran2.sql
@@ -4,7 +4,6 @@ GO
 SELECT * FROM tb_ThuocHienHanh()
 ---
 DECLARE @RT INT
--- EXEC @RT = USP_ThemThuoc 'Alaxan', 7000, N'Thuốc giảm đau kháng viêm', 100, '2025-12-30'
 EXEC @RT = USP_XoaThuoc 'T001'
 IF @RT = 0
 	PRINT N'Xoá thất bại'
